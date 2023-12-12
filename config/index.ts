be47c579576e98e8144632f55aeaf58603ee49a7
Molla Manie Bishaw <54660523+molla-12@users.noreply.github.com>
2023-12-12 15:28:13 +0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,2 @@
-export const MONGO_URI = process.env.MONGO_URL || "mongodb://0.0.0.0:27017/fooddelivery";
+export const MONGO_URI = process.env.MONGO_URL || "mongodb://0.0.0.0:27017/fooddelivery"; //mongo db url
 export const JWT_SECRET = process.env.JWT_SECRET || "secret";
